77a8d29b5afa6411d4b9cd1803f6562406debc66
Koki Takahashi <hakatasiloving@gmail.com>
2014-11-18 09:26:43 +0900

    Add test with snap.svg

--- yayanaru.js
+++ yayanaru.js
@@ -1,10 +1,28 @@
-var socket = io('http://localhost:10001')
+var socket = io('http://localhost:10001');
+
 socket.on('data', function (data) {
 	var acc = JSON.parse(data);
-	$('#circle').css({
-		top: acc.x + 'px',
-		left: acc.y + 'px',
-		width: acc.z / 10 + 'px',
-		height: acc.z / 10 + 'px',
-	});
 });
+
+function onResize(event) {
+	var $svg = $('#svg');
+	var width = Math.min($(window).width(), $(window).height() / 9 * 16);
+	var height = width / 16 * 9;
+
+	$svg.width(width);
+	$svg.height(height);
+	$svg.css({
+		position: 'absolute',
+		top: Math.max(0, (($(window).height() - height) / 2) + $(window).scrollTop()) + 'px',
+		left: Math.max(0, (($(window).width() - width) / 2) + $(window).scrollLeft()) + 'px',
+	});
+}
+
+$(document).ready(function () {
+	onResize();
+	$(window).resize(onResize);
+
+	var svg = Snap('#svg');
+
+	var circle = svg.circle(150, 150, 100);
+})
