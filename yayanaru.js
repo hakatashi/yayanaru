fa42f9ed34723d15f93869c9bce0705d07b851de
Koki Takahashi <hakatasiloving@gmail.com>
2014-11-18 09:35:26 +0900

    Add YouTube Video Player

--- yayanaru.js
+++ yayanaru.js
@@ -5,16 +5,37 @@ socket.on('data', function (data) {
 });
 
 function onResize(event) {
-	var $svg = $('#svg');
+	var $wrap = $('#gamewrapper');
 	var width = Math.min($(window).width(), $(window).height() / 9 * 16);
 	var height = width / 16 * 9;
 
-	$svg.width(width);
-	$svg.height(height);
-	$svg.css({
+	$wrap.width(width);
+	$wrap.height(height);
+	$wrap.css({
 		position: 'absolute',
 		top: Math.max(0, (($(window).height() - height) / 2) + $(window).scrollTop()) + 'px',
 		left: Math.max(0, (($(window).width() - width) / 2) + $(window).scrollLeft()) + 'px',
+	});
+}
+
+function onPlayerReady(event) {
+	event.target.playVideo();
+}
+
+var tag = document.createElement('script');
+tag.src = "https://www.youtube.com/iframe_api";
+var firstScriptTag = document.getElementsByTagName('script')[0];
+firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
+
+var player;
+function onYouTubeIframeAPIReady() {
+	player = new YT.Player('player', {
+		width: '1920',
+		height: '1080',
+		videoId: 'F9TuIVpBi5I',
+		events: {
+			'onReady': onPlayerReady
+		}
 	});
 }
 
