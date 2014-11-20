a61b70c6d8261c621a983979336c8a8dc32acb82
Koki Takahashi <hakatasiloving@gmail.com>
2014-11-21 03:50:25 +0900

    Implement title page

--- yayanaru.js
+++ yayanaru.js
@@ -1,4 +1,18 @@
 var socket = io('http://localhost:10001');
+var svg;
+var title, choices;
+var selectedSong = 0;
+
+var RIGHT = 'l'.charCodeAt(0);
+var LEFT = 'h'.charCodeAt(0);
+
+var songs = [{
+	title: '花ハ踊レヤいろはにほ (難易度: 5)',
+	id: 'F9TuIVpBi5I',
+}, {
+	title: 'YOSAKOIソーラン (難易度: 5)',
+	id: 'F9TuIVpBi5I',
+}];
 
 function onResize(event) {
 	var $wrap = $('#gamewrapper');
@@ -15,7 +29,39 @@ function onResize(event) {
 }
 
 function onPlayerReady(event) {
-	event.target.playVideo();
+	title = svg.image('img/title.jpg', 0, 0);
+	var choice = svg.text(800, 650, songs[selectedSong].title).attr({
+		fill: 'white',
+		'font-size': 72,
+		'text-anchor': 'middle'
+	});
+
+	function onKeypress(event) {
+		if (event.which === RIGHT || event.which === LEFT) {
+			selectedSong = (selectedSong + songs.length + (event.which === RIGHT ? 1 : -1)) % songs.length;
+			var nextSong = songs[selectedSong];
+			var nextChoice = svg.text(event.which === RIGHT ? -800 : 2400, 650, nextSong.title).attr({
+				fill: 'white',
+				'font-size': 72,
+				'text-anchor': 'middle'
+			});
+
+			$(window).unbind('keypress');
+
+			choice.animate({
+				x: event.which === RIGHT ? 2400 : -800
+			}, 300, mina.easeinout);
+			nextChoice.animate({
+				x: 800
+			}, 300, mina.easeinout, function () {
+				$(window).keypress(onKeypress);
+				choice.remove();
+				choice = nextChoice;
+			});
+		}
+	}
+
+	$(window).keypress(onKeypress);
 }
 
 var tag = document.createElement('script');
@@ -39,7 +85,9 @@ $(document).ready(function () {
 	onResize();
 	$(window).resize(onResize);
 
-	var svg = Snap('#svg');
+	$('#player').hide();
+
+	svg = Snap('#svg');
 
 	var pastAcc = null;
 	var pastImpact = new Date(0);
@@ -56,11 +104,9 @@ $(document).ready(function () {
 
 			var impact = Math.sqrt(diff.x * diff.x + diff.y * diff.y + diff.z * diff.z);
 			if (impact > 250 && now - pastImpact > 200) {
-				var impact = svg.rect(0, 0, 1600, 900).attr({fill: 'red'});
-				impact.animate({
-					opacity: 0
-				}, 300, function () {
-					impact.remove();
+				jQuery.event.trigger({
+					type: 'keypress',
+					which: 'l'.charCodeAt(0)
 				});
 				pastImpact = now;
 			}
